c44aca88d327e66c33e38962f957a0b44904fa05
Vignesh Anand <vignesh.anand.1503@gmail.com>
2025-05-25 11:46:20 -0700

    chnaged structure

--- setup.py
+++ setup.py
@@ -4,13 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name="act_test",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(),   # will pick up the act_test directory
     install_requires=[
-        # e.g. "numpy>=1.20", "torch", …
-    ],
-    author="Vignesh Anand",
-    description="ACT-test package",
-    classifiers=[
-        "Programming Language :: Python :: 3",
+        # e.g. "numpy", …
     ],
 )
