1bdf67654f36706b1262e9aeb165b868319ca63d
Vignesh Anand <vignesh.anand.1503@gmail.com>
2025-06-25 21:28:07 +0000

    smoving to docker

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="act_test",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),   # will pick up the act_test directory
     include_package_data=True,
     package_data={"act_test": ["vertex_train.sh"]},
